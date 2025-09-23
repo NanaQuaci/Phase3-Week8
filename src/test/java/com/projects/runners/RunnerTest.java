f05b10625d4b91db5b6e2f8c28205dc4baabf488
Nana Quaci <aducollins49@gmail.com>
2025-09-23 19:33:24 +0000

    Corrected test path

--- src/test/java/com/projects/runners/RunnerTest.java
+++ src/test/java/com/projects/runners/RunnerTest.java
@@ -6,11 +6,9 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 import io.cucumber.junit.platform.engine.Constants;
 
-
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, summary, json:target/cucumber.json, html:target/cucumber.html, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, summary, json:target/cucumber.json")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.projects.stepdefinitions,com.projects.hooks")
-public class RunnerTest {
-}
+public class RunnerTest {}
