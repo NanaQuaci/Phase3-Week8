f3eddd86db75e2fbbde353ff9691c59de0f1afa3
Nana Quaci <aducollins49@gmail.com>
2025-09-29 01:01:20 +0000

    Added cucumber reporting to pipeline

---
+++ generate-cucumber-report.js
@@ -0,0 +1,18 @@
+// generate-cucumber-report.js
+const reporter = require('cucumber-html-reporter');
+
+const options = {
+  theme: 'bootstrap',
+  jsonFile: 'target/cucumber.json',
+  output: 'cucumber-report/index.html',
+  reportSuiteAsScenarios: true,
+  launchReport: false,
+  metadata: {
+    "App Version": "1.0.0",
+    "Test Environment": "GitHub Actions",
+    "Browser": "Chrome",
+    "Platform": "Ubuntu",
+  },
+};
+
+reporter.generate(options);
